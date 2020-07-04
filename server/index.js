339d8fa5a2e38e8d8a04a4afb43f297cbca57fe3
sakshi2912 <sakshiv700@gmail.com>
2020-07-04 15:18:17 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(bodyParser.json());
 
 // Postgres Client Setup
